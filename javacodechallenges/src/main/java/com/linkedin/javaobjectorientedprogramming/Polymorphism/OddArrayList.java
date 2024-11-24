cdde685f8bc433ef36faba07a4956b7be39105dc
Diêgo Fontes <104260227+DigFontes@users.noreply.github.com>
2024-11-24 12:21:32 +0000

    Implemented polymorphism

---
+++ javacodechallenges/src/main/java/com/linkedin/javaobjectorientedprogramming/Polymorphism/OddArrayList.java
@@ -0,0 +1,52 @@
+package com.linkedin.javaobjectorientedprogramming.Polymorphism;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class OddArrayList extends ArrayList<Integer>{
+
+  public OddArrayList(Integer... nums) {
+    super(Arrays.stream(nums)
+            .filter(OddArrayList::isOdd)
+            .collect(Collectors.toList()));
+  }
+  
+  @Override
+  public void add(int index, Integer element) {
+    if (isOdd(element)) {
+      super.add(index, element);
+    }
+  }
+
+
+  @Override
+  public boolean add(Integer element) {
+    if (isOdd(element)) {
+      return super.add(element);
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public boolean addAll(Collection<? extends Integer> c) {
+    return super.addAll(c.stream()
+            .filter(OddArrayList::isOdd)
+            .collect(Collectors.toList())
+            );
+  }
+
+  @Override
+  public boolean addAll(int index, Collection<? extends Integer> c) {
+    return super.addAll(c.stream()
+        .filter(OddArrayList::isOdd)
+        .collect(Collectors.toList()));
+  }
+
+  public static boolean isOdd(Integer element) {
+    return Math.abs(element) % 2 == 1;
+  }
+
+}
